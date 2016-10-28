34b67940e80a5c140b29aeaba730ddfb9cae57d7
Simon Sturmer <sstur@me.com>
2016-10-28 23:11:45 +0700

    Add files

---
+++ plugins.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+module.exports = {
+  'babel-plugin-react-transform': require('babel-plugin-react-transform'),
+  'babel-plugin-syntax-async-functions': require('babel-plugin-syntax-async-functions'),
+  'babel-plugin-syntax-class-properties': require('babel-plugin-syntax-class-properties'),
+  'babel-plugin-syntax-trailing-function-commas': require('babel-plugin-syntax-trailing-function-commas'),
+  'babel-plugin-transform-async-to-generator': require('babel-plugin-transform-async-to-generator'),
+  'babel-plugin-transform-class-properties': require('babel-plugin-transform-class-properties'),
+  'babel-plugin-transform-es2015-destructuring': require('babel-plugin-transform-es2015-destructuring'),
+  'babel-plugin-transform-es2015-modules-commonjs': require('babel-plugin-transform-es2015-modules-commonjs'),
+  'babel-plugin-transform-flow-strip-types': require('babel-plugin-transform-flow-strip-types'),
+  'babel-plugin-transform-object-assign': require('babel-plugin-transform-object-assign'),
+  'babel-plugin-transform-object-rest-spread': require('babel-plugin-transform-object-rest-spread'),
+  'babel-plugin-transform-react-display-name': require('babel-plugin-transform-react-display-name'),
+  'babel-plugin-transform-react-jsx': require('babel-plugin-transform-react-jsx'),
+};
